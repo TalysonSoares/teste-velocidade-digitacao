2d60b015703a37e284a7bc8fa80cefb02ba7abdc
TalysonSoares <talysonsoares40@gmail.com>
2023-05-05 11:49:19 -0300

    Criando exemplos de textos para serem digitados

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const texto = document.querySelector("#texto")
+const entrada = document.querySelector("#entrada")
+const reiniciar = document.querySelector("#reiniciar")
+const resultado = document.querySelector("#resultado")
+const historico = document.querySelector("#historico")
+const alternarTemaBtn = document.querySelector("#alternarTema")
+
+const textos = [
+    "Exemplo de texto para digitar.",
+    "Outro exemplo de texto para digitar.",
+    "Mais um exemplo de texto para digitar.",
+    "Digite isso.",
+    "Você pode digitar isso aqui.",
+];
+
